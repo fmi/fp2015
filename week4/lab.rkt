65bf617aa094477985a046987146681311712bbd
RadoRado <radorado@hackbulgaria.com>
2015-10-26 12:43:24 +0200

    Add missing lab functions

--- week4/lab.rkt
+++ week4/lab.rkt
@@ -6,6 +6,8 @@
   calculate
   twice
   twice2
+  add2
+  add3
   get-operation
   negate)
 
@@ -15,6 +17,13 @@
 (define (twice f x)
   (f (f x)))
 
+; Викаме n поредни пъти функцията f с първи аргумент x
+(define (call-n-times n f x)
+  (cond
+    [(zero? n) x]
+    [else (call-n-times (- n 1) f (f x))]))
+
+; Функции, които връщат функции
 (define (get-operation oper)
   (cond
     [(equal? oper "+") +]
@@ -22,6 +31,15 @@
     [(equal? oper "*") *]
     [(equal? oper "/") /]
     [else +]))
+
+(define (add2 x)
+  (lambda (y)
+    (+ x y)))
+
+(define (add3 x)
+  (lambda (y)
+    (lambda (z)
+      (+ x y z))))
 
 (define (negate p)
   (lambda (x) (not (p x))))
@@ -32,4 +50,3 @@
 (define (twice2 f x)
   ((compose f f) x))
 
-(define (accumulate
