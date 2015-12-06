9fd139ffac6b2bb3d3bb1be4ae8d09bab25f783f
RadoRado <radorado@hackbulgaria.com>
2015-12-06 20:45:21 +0200

    Fix wrong solution of 1st problem from 2nd group

--- exam1/version2-problem1.rkt
+++ exam1/version2-problem1.rkt
@@ -12,20 +12,23 @@
 (define (divisible? x y)
   (= 0 (remainder x y)))
 
-(define (sum-digits n)
-  (sum (map string->number
-            (map ~a (string->list (~a n))))))
+(define (sum-divisors n)
+  (define (helper i)
+    (if (= i n)
+        i
+        (+ (if (= 0 (remainder n i)) i 0) (helper (add1 i)))))
+  (helper 1))
 
 (define (product-sum-div a b k)
   (product (filter (lambda (x)
-                     (divisible? (sum-digits x) k))
+                     (divisible? (sum-divisors x) k))
                    (range a (add1 b)))))
 
 (run-tests
  (test-suite
   "Testing first problem"
-  (check-equal? (product-sum-div 1 10 1) 3628800 "When k = 1, we get the product of all ints between a and b, inclusive")
-  (check-equal? (product-sum-div 1 10 2) 384 "When k = 2, we get the product of all even numbers between 1 and 9. 10 is not included, becasue 1 + 0 % 2 != 0")
-  (check-equal? (product-sum-div 1 10 12) 1 "When k > b, we get 1.")
-  (check-equal? (product-sum-div 1 100 7) 2294089349563531520000 "A larger example")
+  (check-equal? (product-sum-div 1 10 1) 3628800)
+  (check-equal? (product-sum-div 1 10 2) 6300)
+  (check-equal? (product-sum-div 1 10 12) 6)
+  (check-equal? (product-sum-div 1 100 7) 268304563877986508606716826418256281600000)
   ))
