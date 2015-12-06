8bc094a8925dfd1169f1126b219e02eef8524bcb
RadoRado <radorado@hackbulgaria.com>
2015-12-06 20:25:19 +0200

    Fix name of 1st problem of 2nd group

--- exam1/version2-problem1.rkt
+++ exam1/version2-problem1.rkt
@@ -16,7 +16,7 @@
   (sum (map string->number
             (map ~a (string->list (~a n))))))
 
-(define (sum-sum-digit a b k)
+(define (product-sum-div a b k)
   (product (filter (lambda (x)
                      (divisible? (sum-digits x) k))
                    (range a (add1 b)))))
@@ -24,8 +24,8 @@
 (run-tests
  (test-suite
   "Testing first problem"
-  (check-equal? (sum-sum-digit 1 10 1) 3628800 "When k = 1, we get the product of all ints between a and b, inclusive")
-  (check-equal? (sum-sum-digit 1 10 2) 384 "When k = 2, we get the product of all even numbers between 1 and 9. 10 is not included, becasue 1 + 0 % 2 != 0")
-  (check-equal? (sum-sum-digit 1 10 12) 1 "When k > b, we get 1.")
-  (check-equal? (sum-sum-digit 1 100 7) 2294089349563531520000 "A larger example")
+  (check-equal? (product-sum-div 1 10 1) 3628800 "When k = 1, we get the product of all ints between a and b, inclusive")
+  (check-equal? (product-sum-div 1 10 2) 384 "When k = 2, we get the product of all even numbers between 1 and 9. 10 is not included, becasue 1 + 0 % 2 != 0")
+  (check-equal? (product-sum-div 1 10 12) 1 "When k > b, we get 1.")
+  (check-equal? (product-sum-div 1 100 7) 2294089349563531520000 "A larger example")
   ))
